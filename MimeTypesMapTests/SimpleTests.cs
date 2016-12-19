6d988e6ce6af8081780fa7744992d7ecb6b02783
red <hey.red@outlook.com>
2016-12-19 20:55:30 +0300

    Add sources

---
+++ MimeTypesMapTests/SimpleTests.cs
@@ -0,0 +1,29 @@
+﻿using HeyRed.Mime;
+using Xunit;
+
+namespace MimeTypesMapTests
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public void GetMimeTypeByFileName()
+        {
+            var mime = MimeTypesMap.GetMimeType("test.jpeg");
+            Assert.Equal("image/jpeg", mime);
+        }
+
+        [Fact]
+        public void GetMimeTypeByExtension()
+        {
+            var mime = MimeTypesMap.GetMimeType("jpeg");
+            Assert.Equal("image/jpeg", mime);
+        }
+
+        [Fact]
+        public void GetExtension()
+        {
+            var ext = MimeTypesMap.GetExtension("image/jpeg");
+            Assert.Equal("jpeg", ext);
+        }
+    }
+}
